9c13d114438094b5681a9aa5fea1d70139f995c9
nellaparedes <nellaparedes@users.noreply.github.com>
2015-06-18 14:13:34 -0500

    plugin files

---
+++ acf-form-v4.php
@@ -0,0 +1,149 @@
+<?php
+
+class acf_field_form extends acf_field {
+	
+	// vars
+	var $settings, // will hold info such as dir / path
+		$defaults; // will hold default field options
+		
+		
+	/*
+	*  __construct
+	*
+	*  Set name / label needed for actions / filters
+	*
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+	
+	function __construct()
+	{
+		// vars
+		$this->name = 'form';
+		$this->label = __('Wordpress Form Manager Shortcode');
+		$this->category = __("Choice",'acf'); // Basic, Content, Choice, etc
+		$this->defaults = array(
+			// add default here to merge into your field. 
+			// This makes life easy when creating the field options as you don't need to use any if( isset('') ) logic. eg:
+			//'preview_size' => 'thumbnail'
+		);
+		
+		
+		// do not delete!
+    	parent::__construct();
+    	
+    	
+    	// settings
+		$this->settings = array(
+			'path' => apply_filters('acf/helpers/get_path', __FILE__),
+			'dir' => apply_filters('acf/helpers/get_dir', __FILE__),
+			'version' => '1.0.0'
+		);
+
+	}
+	
+	
+	/*
+	*  create_options()
+	*
+	*  Create extra options for your field. This is rendered when editing a field.
+	*  The value of $field['name'] can be used (like below) to save extra data to the $field
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field	- an array holding all the field's data
+	*/
+	
+	function create_options( $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		
+		// key is needed in the field names to correctly save the data
+		$key = $field['name'];
+		
+		
+		// Create Field Options HTML
+		?>
+		<?php
+		
+	}
+	
+	
+	/*
+	*  create_field()
+	*
+	*  Create the HTML interface for your field
+	*
+	*  @param	$field - an array holding all the field's data
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+	
+	function create_field( $field )
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		
+		// perhaps use $field['preview_size'] to alter the markup?
+		
+		
+		// create Field HTML
+
+		global $fmdb;
+		if(!isset($fmdb))
+		{
+			echo 'Debe tener activado el plugin Wordpress Form Manager para usar este campo';
+			return;
+		}
+
+		$results = $fmdb->getFormList();
+		$value = $field['value'];
+		?>
+		<div>			
+			<select id="<?php echo $field['id']; ?>" class="<?php echo $field['class']; ?>" name="<?php echo esc_attr($field['name']) ?>" >
+				<?php //loop for results
+	            foreach($results as $form){
+	                ?>
+	                <option value='<?php echo $form['shortcode']?>' <?php if($value == $form['shortcode']) echo 'selected'; ?>>
+	                	<?php echo $form['title']?>
+	                </option>
+	                <?php
+	            }
+	            ?>
+	        </select>
+		</div>
+		<?php
+	}
+	
+	
+	/*
+	*  input_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
+	*  Use this action to add CSS + JavaScript to assist your create_field() action.
+	*
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function input_admin_enqueue_scripts()
+	{		
+	}
+}
+
+
+// create field
+new acf_field_form();
+
+?>
